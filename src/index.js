fee8ad8d01d65ac7aa7f5e3b97c2fde9d60f08e8
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-07-20 13:09:04 +0200

    chat kinda almost no?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import mongoose from "mongoose"
 import server from "./server.js"
+import { createServer } from "http"
+
+export const http = createServer(server)
+import { Server } from "socket.io"
+
+const io = new Server(http, { allowEIO3: true })
+
+let onlineUsers = []
+
+// Add "event listeners" on your socket when it's connecting
+io.on("connection", socket => {
+    // socket.on("join-room", (room) => {
+    //     socket.join(room)
+    //     console.log(socket.rooms)
+    // })
+
+    socket.on("setUsername", ({ username, room }) => {
+        onlineUsers.push({ username: username, id: socket.id, room })
+
+        //.emit - echoing back to itself
+        socket.emit("loggedin")
+
+        //.broadcast.emit - emitting to everyone else
+        socket.broadcast.emit("newConnection")
+        socket.join(room)
+
+        //io.sockets.emit - emitting to everybody in the known world
+        //io.sockets.emit("newConnection")
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Disconnecting...")
+        onlineUsers = onlineUsers.filter(user => user.id !== socket.id)
+    })
+
+    socket.on("sendMessage", async ({ message, room }) => {
+        await Model.findOneAndUpdate({ name: room }, { $push: { chatHistory: message } })
+        socket.to(room).emit("message", message)
+    })
+})
 
 process.env.TS_NODE_DEV && require("dotenv").config()
 const port = process.env.PORT || 3030
@@ -9,5 +49,5 @@ if (!MONGO_CONNECTION) throw new Error("No Mongo DB specified")
 
 mongoose
     .connect(MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(() => server.listen(port, () => console.log("Server running on port", port)))
+    .then(() => http.listen(port, () => console.log("Server running on port", port)))
     .catch(e => console.log(e))
--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ import cookieParser from "cookie-parser"
 import usersRoutes from "./services/users/index.js"
 import chatRoutes from "./services/chat/index.js"
 import { catchAllHandler, error4xx } from "./errors.js"
+import { JWTAuthMiddleware } from "./auth/middlewares.js"
 
 const server = express()
-
 // MIDDLEWARES
 
 server.use(cors({ origin: "localhost", credentials: true }))
@@ -20,7 +20,7 @@ server.use(csrf({ cookie: { httpOnly: true /*, sameSite: "lax", secure: true*/ }
 
 // ROUTES
 server.use("/users", usersRoutes)
-//server.use("/chat", chatRoutes)
+server.use("/chat", JWTAuthMiddleware, chatRoutes)
 
 // ERROR HANDLERS
 server.use(error4xx)
